eca17f9dd28dfca7255eff5ab6e815846d5aa36c
Konstantin <konstantin.klein+github@hochfrequenz.de>
2024-10-11 07:04:08 +0200

    fix strict nullability in few files

--- MaLoIdentModels/MaLoIdentModels/GeographicCoordinates.cs
+++ MaLoIdentModels/MaLoIdentModels/GeographicCoordinates.cs
@@ -1,28 +1,28 @@
 using System.Text.Json.Serialization;
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
 namespace MaLoIdentModels;
 
 public class GeographicCoordinates
 {
     [JsonPropertyName("latitude")]
-    public string Latitude { get; set; }
+    public string? Latitude { get; set; }
 
     [JsonPropertyName("longitude")]
-    public string Longitude { get; set; }
+    public string? Longitude { get; set; }
 
     [JsonPropertyName("east")]
-    public string East { get; set; }
+    public string? East { get; set; }
 
     [JsonPropertyName("north")]
-    public string North { get; set; }
+    public string? North { get; set; }
 
     [JsonConverter(typeof(JsonStringEnumConverter))]
     [JsonPropertyName("zone")]
     public Zone? Zone { get; set; }
 
     [JsonPropertyName("northing")]
-    public string Northing { get; set; }
+    public string? Northing { get; set; }
 
     [JsonPropertyName("easting")]
-    public string Easting { get; set; }
+    public string? Easting { get; set; }
 }
