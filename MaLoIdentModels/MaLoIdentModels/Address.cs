f3f27887d251e886fc2228979fa223690b3fb130
konstantin <konstantin.klein@hochfrequenz.de>
2024-10-11 07:08:31 +0200

    fix strict nullability in few files (#16)
    * fix strict nullability in few files
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- MaLoIdentModels/MaLoIdentModels/Address.cs
+++ MaLoIdentModels/MaLoIdentModels/Address.cs
@@ -1,4 +1,3 @@
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 namespace MaLoIdentModels;
 
 using System.ComponentModel.DataAnnotations;
@@ -8,20 +7,20 @@ public class Address
 {
     [JsonPropertyName("countryCode")]
     [RegularExpression(@"[A-Z]{2}")]
-    public string CountryCode { get; set; }
+    public string? CountryCode { get; set; }
 
     [JsonPropertyName("zipCode")]
-    public string ZipCode { get; set; }
+    public string? ZipCode { get; set; }
 
     [JsonPropertyName("city")]
-    public string City { get; set; }
+    public string? City { get; set; }
 
     [JsonPropertyName("street")]
-    public string Street { get; set; }
+    public string? Street { get; set; }
 
     [JsonPropertyName("houseNumber")]
     public int HouseNumber { get; set; }
 
     [JsonPropertyName("houseNumberAddition")]
-    public string HouseNumberAddition { get; set; }
+    public string? HouseNumberAddition { get; set; }
 }
--- MaLoIdentModels/MaLoIdentModels/MarketLocationDateTime.cs
+++ MaLoIdentModels/MaLoIdentModels/MarketLocationDateTime.cs
@@ -3,14 +3,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using MaLoIdentModels.JsonSettings;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 namespace MaLoIdentModels;
 
 public class MarketLocationDateTime
 {
     [JsonPropertyName("maloId")]
     [RegularExpression(@"\d{11}")]
-    public string MaloId { get; set; }
+    public string? MaloId { get; set; }
 
     [JsonConverter(typeof(DateTimeOffsetWithTrailingZConverter))]
     [JsonPropertyName("executionTimeFrom")]
