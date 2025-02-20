d0555d60fca06058efa7a39b28702153b742e69c
JonasSchneegans <jonas.schneegans@hochfrequenz.de>
2025-02-20 11:35:56 +0100

    add empty string converter

--- MaLoIdentModels/MaLoIdentModels/v1/GeographicCoordinates.cs
+++ MaLoIdentModels/MaLoIdentModels/v1/GeographicCoordinates.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using MaLoIdentModels.JsonSettings;
 
 namespace MaLoIdentModels.v1;
 
@@ -8,25 +9,31 @@ public class GeographicCoordinates
     [System.ComponentModel.DataAnnotations.Key]
     public System.Guid? Id { get; set; }
 
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("latitude")]
     public string? Latitude { get; set; }
-
+    
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("longitude")]
     public string? Longitude { get; set; }
 
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("east")]
     public string? East { get; set; }
 
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("north")]
     public string? North { get; set; }
-
+    
     [JsonConverter(typeof(JsonStringEnumConverter))]
     [JsonPropertyName("zone")]
     public Zone? Zone { get; set; }
 
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("northing")]
     public string? Northing { get; set; }
 
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("easting")]
     public string? Easting { get; set; }
 }
---
+++ MaLoIdentModels/MaLoIdentModelsTests/v1Tests/EmptyStringAndObjectsDeserializationTests.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using FluentAssertions;
+using MaLoIdentModels.v1;
+
+namespace MaLoIdentModelsTests.v1Tests;
+
+public class EmptyStringAndObjectsDeserializationTests
+{
+    [Fact]
+    public void Deserializing_An_DatetimeOffset_Without_Explicit_Offset_Throws_JsonException()
+    {
+        var fileBody = File.ReadAllText("v1Tests/examples/request_empty_string.json");
+        fileBody
+            .Should()
+            .Contain(
+                "\"\"",
+                because: "In the testdata an empty string is used to represent a missing value"
+            );
+        var deserializing = () => JsonSerializer.Deserialize<IdentificationParameter>(fileBody);
+        var deserializedModel = deserializing();
+        deserializedModel.Should().NotBeNull();
+        deserializedModel.IdentificationParameterAddress.Should().NotBeNull();
+        deserializedModel.IdentificationParameterAddress.GeographicCoordinates.Should().NotBeNull();
+        deserializedModel
+            .IdentificationParameterAddress.GeographicCoordinates.Easting.Should()
+            .BeNull();
+        deserializedModel
+            .IdentificationParameterAddress.GeographicCoordinates.Northing.Should()
+            .BeNull();
+        deserializedModel
+            .IdentificationParameterAddress.GeographicCoordinates.East.Should()
+            .BeNull();
+        deserializedModel
+            .IdentificationParameterAddress.GeographicCoordinates.North.Should()
+            .BeNull();
+    }
+}
