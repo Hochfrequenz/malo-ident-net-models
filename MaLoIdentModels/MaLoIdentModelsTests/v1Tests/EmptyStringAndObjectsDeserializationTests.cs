a23f789e9dbcee830da4e48bffd95ce5f06eea62
JonasSchneegans <jonas.schneegans@hochfrequenz.de>
2025-02-25 11:06:42 +0100

    add comments

--- MaLoIdentModels/MaLoIdentModelsTests/v1Tests/EmptyStringAndObjectsDeserializationTests.cs
+++ MaLoIdentModels/MaLoIdentModelsTests/v1Tests/EmptyStringAndObjectsDeserializationTests.cs
@@ -7,7 +7,7 @@ namespace MaLoIdentModelsTests.v1Tests;
 public class EmptyStringAndObjectsDeserializationTests
 {
     [Fact]
-    public void Deserializing_An_DatetimeOffset_Without_Explicit_Offset_Throws_JsonException()
+    public void Deserializing_Objects_With_Empty_Strings()
     {
         var fileBody = File.ReadAllText("v1Tests/examples/request_empty_string.json");
         fileBody
@@ -19,8 +19,16 @@ public class EmptyStringAndObjectsDeserializationTests
         var deserializing = () => JsonSerializer.Deserialize<IdentificationParameter>(fileBody);
         var deserializedModel = deserializing();
         deserializedModel.Should().NotBeNull();
+        deserializedModel.IdentificationParameterId.Should().NotBeNull();
         deserializedModel.IdentificationParameterAddress.Should().NotBeNull();
+        // the field tranchenIds is an empty list in the test data and should therefore be null after deserializing
+        deserializedModel.IdentificationParameterId.TranchenIds.Should().BeNull();
+        // Landparcels should be null, because in the test data the fields of all objects in this list,
+        // are empty strings
+        deserializedModel.IdentificationParameterAddress.LandParcels.Should().BeNull();
         deserializedModel.IdentificationParameterAddress.GeographicCoordinates.Should().NotBeNull();
+        // the following fields of the geographic coordinates object should be null, because in the test data the fields
+        // are empty strings
         deserializedModel
             .IdentificationParameterAddress.GeographicCoordinates.Easting.Should()
             .BeNull();
