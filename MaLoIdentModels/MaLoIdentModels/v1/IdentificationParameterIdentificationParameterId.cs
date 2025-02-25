4796a337ead6fb1b62a7b3865c0a69c5cbc6fda9
JonasSchneegans <jonas.schneegans@hochfrequenz.de>
2025-02-25 10:18:06 +0100

    Add converter to IdentificationParameterId

--- MaLoIdentModels/MaLoIdentModels/v1/IdentificationParameterIdentificationParameterId.cs
+++ MaLoIdentModels/MaLoIdentModels/v1/IdentificationParameterIdentificationParameterId.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using MaLoIdentModels.JsonSettings;
 
 namespace MaLoIdentModels.v1;
 
@@ -10,18 +11,23 @@ public class IdentificationParameterIdentificationParameterId
     [Key]
     public System.Guid? Id { get; set; }
 
+    [JsonConverter(typeof(EmptyStringConverter))]
     [JsonPropertyName("maloId")]
     public string? MaloId { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("tranchenIds")]
     public List<string>? TranchenIds { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("meloIds")]
     public List<string>? MeloIds { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("meterNumbers")]
     public List<string>? MeterNumbers { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("customerNumber")]
     public string? CustomerNumber { get; set; }
 }
