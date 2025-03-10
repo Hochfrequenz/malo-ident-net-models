82161bdf556f5e11869b933c45c64988637f3411
JonasSchneegans <jonas.schneegans@hochfrequenz.de>
2025-03-10 10:34:28 +0100

    add empty string to null converter for name fields

--- MaLoIdentModels/MaLoIdentModels/v1/Name.cs
+++ MaLoIdentModels/MaLoIdentModels/v1/Name.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using MaLoIdentModels.JsonSettings;
 
 namespace MaLoIdentModels.v1;
 
@@ -8,15 +9,19 @@ public class Name
     [System.ComponentModel.DataAnnotations.Key]
     public System.Guid? Id { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("surnames")]
     public string? Surnames { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("firstnames")]
     public string? Firstnames { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("title")]
     public string? Title { get; set; }
 
+    [JsonConverter(typeof(EmptyListToNullConverter<string>))]
     [JsonPropertyName("company")]
     public string? Company { get; set; }
 }
